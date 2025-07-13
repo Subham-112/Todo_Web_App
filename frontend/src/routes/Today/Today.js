83bc9d84524dbbafa3236015893addf08498e0d2
Subham Sahu <subhambusiness286@gmail.com>
2025-07-13 15:37:08 +0530

    Added the Hid nav feature in Today component

--- frontend/src/routes/Today/Today.js
+++ frontend/src/routes/Today/Today.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "../../Style/Today.css";
 import TodayContent from "./Today_Content/TodayContent";
 import TodayNav from "./TodayNav";
 
 export default function Today({ hidNav }) {
+  let [hideNav, setHideNav] = useState(false);
+
+  const handleHideNav = () => {
+    setHideNav(!hideNav);
+  }
+
   return (
-    <div id="today">
-      <div className="pr-td-nv"><TodayNav /></div>
-      <div className="pr-td-cont"><TodayContent hidNav={hidNav} /></div>
+    <div id="today" className={!hideNav ? `grid-class` : `flex-class`}>
+      <div className={!hideNav ? `pr-td-nv` : `hid-td-nv`}>
+        <TodayNav />
+      </div>
+      <div className={!hideNav ? `symbol` : `the-symbol`} onClick={handleHideNav}>
+        <p>&lt;</p>
+      </div>
+      <div className={!hideNav ? `pr-td-cont` : `hid-td-cont`}>
+        <TodayContent hidNav={hidNav} />
+      </div>
     </div>
   );
 }
