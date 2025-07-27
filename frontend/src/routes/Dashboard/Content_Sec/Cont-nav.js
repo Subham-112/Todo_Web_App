650aebd3dd8485015cc2a0da453d53b16979091f
Subham Sahu <subhambusiness286@gmail.com>
2025-07-27 23:07:49 +0530

    Added Logout Feature on Dashboard

--- frontend/src/routes/Dashboard/Content_Sec/Cont-nav.js
+++ frontend/src/routes/Dashboard/Content_Sec/Cont-nav.js
@@ -1,38 +1,67 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import { successMsg } from '../../../utils'
 
 export default function ContNav() {
-    let [ name, setName ] = useState("");
-    useEffect(() => {
-        const getName = () => {
-            const name = localStorage.getItem("User");
-            setName(name);
-        }
-        getName();
-    }, []);
+  let [name, setName] = useState("");
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const getName = () => {
+      const name = localStorage.getItem("User");
+      setName(name);
+    };
+    getName();
+  }, []);
+  
+  const handleLogout = () => {
+    localStorage.removeItem('Jwt_Token');
+    localStorage.removeItem('User');
+    successMsg("Logout successful");
+    setTimeout(() => {
+      navigate('/login')
+    }, 2000);
+  }
+
   return (
-    <div id="Cont-nav" className="w-100 d-flex justify-content-between align-items-center">
+    <div
+      id="Cont-nav"
+      className="w-100 d-flex justify-content-between align-items-center"
+    >
       <div className="ms-5">
-        <h1 style={{ color: '#00ffd9ff', textShadow: '0px 0px 15px #00ffd9ff' }}><b>Welcome @{name}</b></h1>
+        <h1
+          style={{ color: "#00ffd9ff", textShadow: "0px 0px 15px #00ffd9ff" }}
+        >
+          <b>Welcome @{name}</b>
+        </h1>
       </div>
       <div className="me-5 d-flex gap-3">
         <button
           style={{
-            border: '3px solid #00ffd9ff',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            color: '#00ffd9ff',
-            fontSize: '1.1rem',
-            fontWeight: '600'
+            border: "3px solid #00ffd9ff",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            color: "#00ffd9ff",
+            fontSize: "1.1rem",
+            fontWeight: "600",
           }}
-          className="p-2 ps-3 pe-3 rounded-3">Logout</button>
+          className="p-2 ps-3 pe-3 rounded-3"
+          onClick={() => handleLogout()}
+        >
+          Logout
+        </button>
         <button
           style={{
-            border: '3px solid #00ffd9ff',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            color: '#00ffd9ff',
-            fontSize: '1.1rem',
-            fontWeight: '600'
+            border: "3px solid #00ffd9ff",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            color: "#00ffd9ff",
+            fontSize: "1.1rem",
+            fontWeight: "600",
           }}
-          className="p-2 ps-3 pe-3 rounded-3">@{name}</button>
+          className="p-2 ps-3 pe-3 rounded-3"
+        >
+          @{name}
+        </button>
         <span className="d-none">Accoun Info</span>
       </div>
     </div>
