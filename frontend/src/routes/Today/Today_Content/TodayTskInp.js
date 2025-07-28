ac62079a963302ac6e85f31f5d14ff54f21678cb
Subham Sahu <subhambusiness286@gmail.com>
2025-07-28 16:26:07 +0530

    Added some styling on Date section

--- frontend/src/routes/Today/Today_Content/TodayTskInp.js
+++ frontend/src/routes/Today/Today_Content/TodayTskInp.js
@@ -1,4 +1,4 @@
-import { DatePicker } from "antd";
+import { DatePicker, Flex } from "antd";
 import { useEffect, useState } from "react";
 
 export default function TodayTaskInp({
@@ -28,18 +28,18 @@ export default function TodayTaskInp({
 
   useEffect(() => {
     const senDate = () => {
-      if(pickDate) {
+      if (pickDate) {
         getDate(date.now);
       } else {
         getDate(date.custom);
       }
-    }
+    };
     senDate();
-  }, [date, pickDate, getDate])
+  }, [date, pickDate, getDate]);
 
   useEffect(() => {
     getTask(task);
-  }, [task, getTask])
+  }, [task, getTask]);
 
   const handleChange = (date, dateStr) => {
     setDate((prev) => ({ ...prev, custom: dateStr }));
@@ -52,62 +52,85 @@ export default function TodayTaskInp({
 
   const handleWrong = () => {
     setClearDate(!clearDate);
-    setDate(prev => ({ ...prev, custom: '' }));
+    setDate((prev) => ({ ...prev, custom: "" }));
     setBtn(true);
   };
 
   return (
     <div className="ch-td-inp">
       {pickDate ? (
-        <div className="date">
-          <p>{date.now}</p>
-          <button onClick={() => setPickDate(!pickDate)}>Custom Date</button>
+        <div className="date-today">
+          <span
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/external-icongeek26-flat-icongeek26/45/external-Date-romance-and-love-icongeek26-flat-icongeek26.png"
+              alt="external-Date-romance-and-love-icongeek26-flat-icongeek26"
+              style={{ filter: "drop-shadow(0px 0px 10px black)" }}
+            />
+            <p style={{ marginTop: "1rem", textShadow: "0px 0px 10px black" }}>
+              {date.now}
+            </p>
+          </span>
+          <button className="custom" onClick={() => setPickDate(!pickDate)}>
+            Custom Date
+          </button>
         </div>
       ) : (
-        <div className="date">
-          {!date.custom ? (
-            <p>Enter your date 👉</p>
-          ) : (
+        <div className="date-custom">
+          <div className="date">
+            {!date.custom ? (
+              <p>Enter your date 👉</p>
+            ) : (
+              <span>
+                <p>{date.custom}</p>
+              </span>
+            )}
             <span>
-              <p>{date.custom}</p>
-            </span>
-          )}
-          <span>
-            {!clearDate ? (
-              <DatePicker
-                onChange={handleChange}
-                className="custom-datepicker"
-                placeholder="Enter Date"
-              />
-            ) : (
-              <img
-                width="45"
-                height="45"
-                src="https://img.icons8.com/arcade/64/delete-sign.png"
-                alt="delete-sign"
-                style={{ cursor: "pointer" }}
-                onClick={() => handleWrong()}
-              />
-            )}
+              {!clearDate ? (
+                <DatePicker
+                  onChange={handleChange}
+                  className="custom-datepicker"
+                  placeholder="Enter Date"
+                />
+              ) : (
+                <img
+                  width="45"
+                  height="45"
+                  src="https://img.icons8.com/arcade/64/delete-sign.png"
+                  alt="delete-sign"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleWrong()}
+                />
+              )}
 
-            {clearDate && btn ? (
-              <img
-                width="45"
-                height="45"
-                src="https://img.icons8.com/doodle/48/checkmark.png"
-                alt="checkmark"
-                style={{
-                  marginLeft: "1.5rem",
-                  cursor: "pointer",
-                }}
-                onClick={() => handleTick()}
-              />
-            ) : (
-              <></>
-            )}
-          </span>
+              {clearDate && btn ? (
+                <img
+                  width="45"
+                  height="45"
+                  src="https://img.icons8.com/doodle/48/checkmark.png"
+                  alt="checkmark"
+                  style={{
+                    marginLeft: "1.5rem",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleTick()}
+                />
+              ) : (
+                <></>
+              )}
+            </span>
+          </div>
           <span>
-            <button onClick={() => setPickDate(!pickDate)}>Today</button>
+            <button className="today-dt" onClick={() => setPickDate(!pickDate)}>
+              Today
+            </button>
           </span>
         </div>
       )}
@@ -121,7 +144,7 @@ export default function TodayTaskInp({
         onChange={(e) => setTask(e.target.value)}
         onKeyDown={(e) => {
           handleInfoSubmit(e);
-          setTask('')
+          setTask("");
         }}
         autoFocus
       />
